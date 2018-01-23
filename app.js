083dce99796adf1593eb0f2b7d8f4ccd6f8624cf
Robert Stow <robstow94@gmail.com>
2018-01-23 10:03:11 +0000

    Remove useless code.

--- app.js
+++ app.js
@@ -5,23 +5,6 @@ const users = [{"email": "robstow94@gmail.com", "roles": ["user", "admin"]}];
 
 app.use('/api', require('./api'));
 
-// you can put your client ID here
-app.use(googleauth("511406985315-rl37rcsg10p64jn3aejk6i52t9ior0bv.apps.googleusercontent.com"));
-
-// you can put your realm here instead of 'jwt'
-// return 'Not authorized' if we don't have a user
-app.use('/api', googleauth.guardMiddleware({realm: 'jwt'}));
-
-app.get('/api/protected', function (req, res) {
-  if (req.user.displayName) {
-    res.send('Hello ' + req.user.displayName + '!');
-  } else {
-    res.send('Hello stranger!');
-  }
-
-  console.log('successful authorized request by ' + req.user.emails[0].value);
-});
-
 app.use(express.static('static', { extensions: ['html'] }));
 
 const port = process.env.PORT || 8080;
